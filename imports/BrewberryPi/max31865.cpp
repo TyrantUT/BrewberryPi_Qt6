42a0aacba1092e4d65856d84e15f807315d13886
Christopher Radoumis <tyrantut@domain.local>
2024-09-28 17:12:27 -0700

    sleeping .5 sec

--- imports/BrewberryPi/max31865.cpp
+++ imports/BrewberryPi/max31865.cpp
@@ -77,7 +77,7 @@ void MAX31865::MAX31865_readTemp(void) {
 
     // We need to allow for at least 100msec for each conversion
     // Note: This will impact the Temp Thread overall wait time since all 4 are within 1 thread
-    QThread::msleep(100);
+    QThread::msleep(500);
 }
 
 void MAX31865::MAX31865_writeRegister(quint8 regNum, quint8 data) {
@@ -116,7 +116,7 @@ void MAX31865::MAX31865_calculateTempC(quint16 rtd_response) {
     temp = Z2 + (Z3 * Rt);
     temp = (sqrt(temp) + Z1) / Z4;
 
-    printf("Temp in C: %f\n", temp);
+    //printf("Temp in C: %f\n", temp);
 
     temp = MAX31865_normalizeTemp(temp);
     {
